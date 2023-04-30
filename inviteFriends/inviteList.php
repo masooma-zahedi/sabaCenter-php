2dd967843776b2b59b37ca9296a7471b5d200943
Masooma <masooma.zahedi1@gmail.com>
2023-04-30 11:41:24 -0500

    make 'member page'

--- inviteFriends/inviteList.php
+++ inviteFriends/inviteList.php
@@ -17,30 +17,32 @@ $result = mysqli_query($conn, $sql);
 
 <body>
     <div class="container">
-        <h2 class="m-5">Invite Friends</h2>
+        <div class="card my-5 ">
+            <div class="card-body h3 text-primary mx-auto"> Please fill out the form below and we’ll be in touch. </div>
+        </div>
         <table class="table text-center border">
-            <thead >
+            <thead>
                 <tr>
-                    <th class="border"  scope="col">use_id</th>
-                    <th class="border"  scope="col">Name</th>
-                    <th class="border"  scope="col">Last name</th>
-                    <th class="border"  scope="col">Email</th>
-                    <th class="border"  scope="col">numPeople</th>
+                    <th class="border" scope="col">use_id</th>
+                    <th class="border" scope="col">Name</th>
+                    <th class="border" scope="col">Last name</th>
+                    <th class="border" scope="col">Email</th>
+                    <th class="border" scope="col">numPeople</th>
                 </tr>
             </thead>
             <tbody>
                 <?php
-                    while($row = mysqli_fetch_assoc($result)){
+                while ($row = mysqli_fetch_assoc($result)) {
                 ?>
-                <tr>
-                    <th class="border"  scope="row"><?php echo $row["user_id"] ?> </th>
-                    <td class="border" ><?php echo $row["name"] ?></td>
-                    <td class="border" ><?php echo $row["lastname"] ?></td>
-                    <td class="border" ><?php echo $row["email"] ?></td>
-                    <td class="border" ><a href="listfriends.php?email=<?php echo $row['email'] ?> "><?php echo $row["numPeople"] ?> </a></td>
-                </tr>
+                    <tr>
+                        <th class="border" scope="row"><?php echo $row["user_id"] ?> </th>
+                        <td class="border"><?php echo $row["name"] ?></td>
+                        <td class="border"><?php echo $row["lastname"] ?></td>
+                        <td class="border"><?php echo $row["email"] ?></td>
+                        <td class="border"><a href="listfriends.php?email=<?php echo $row['email'] ?> "><?php echo $row["numPeople"] ?> </a></td>
+                    </tr>
                 <?php
-                    }
+                }
                 ?>
             </tbody>
         </table>
@@ -48,9 +50,9 @@ $result = mysqli_query($conn, $sql);
 
 
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 </body>
 
---
+++ members/members.php
@@ -0,0 +1,98 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "php_test");
+$sql = "SELECT * FROM members_tbl";
+$res = mysqli_query($conn, $sql);
+// while ($row = mysqli_fetch_assoc($res)) {
+//     var_dump($res) . "<br/>";
+// }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>members</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+</head>
+
+<body>
+        <!-- header -->
+        <div class="header container-fluid" style="background-color :#e3f2fd">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light h2">
+                <a class="navbar-brand" href="../sabacenter.php">Saba Center</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse h5" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item ">
+                            <a class="nav-link" href="../sabacenter.php">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../contactF/formContact.php">Contact Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="../inviteFriends/sabaInviter.php">Invite Friends</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="#">Donate</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link " href="../login/login.php">Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <!-- end header -->
+
+    <div class="container">
+        <div class="card my-5 ">
+            <div class="card-body h3 text-primary mx-auto"> Members </div>
+        </div>
+        <!-- start table's members -->
+        <table class="table text-center border">
+            <thead>
+                <tr>
+                    <th class="border" scope="col">use_id</th>
+                    <th class="border" scope="col">Name</th>
+                    <th class="border" scope="col">Last name</th>
+                    <th class="border" scope="col">numPeople</th>
+                    <th class="border" scope="col">Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                while ($row = mysqli_fetch_assoc($res)) {
+                ?>
+                    <tr>
+                        <th class="border" scope="row"><?php echo $row["user_id"] ?> </th>
+                        <td class="border"><?php echo $row["name"] ?></td>
+                        <td class="border"><?php echo $row["lastname"] ?></td>
+                        <td class="border"><?php echo $row["phone"] ?> </td>
+                        <td class="border"><?php echo $row["email"] ?></td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+        <!-- end table's members -->
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
